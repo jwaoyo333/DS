32f55814f5936efb5b7599585d5648e26646538b
jwaoyo333 <165109274+jwaoyo333@users.noreply.github.com>
2024-10-07 16:22:51 +0900

    Update stackproblem.c

--- 1007/stackproblem.c
+++ 1007/stackproblem.c
@@ -2,26 +2,27 @@
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
 
 typedef enum {
 	lparen, rparen, plus, minus, times, divide, mod, eos, lg, rg, and, or , operand
 }precedence; // 열거형 변수 선언
+
 int stack[100];
-char expr[100];
 char symbol;
 int n = 0;
 int top = 0;
 
-void push(int token) { // token을 받아 stack에 추가하는 함수
+void push(int token) { // stack에 피연산자를 추가하는 함수
 	stack[++top] = token;
 	return;
 }
 
 int pop() {
-	return stack[top--]; // stack에서 precedence 타입의 token을 출력하는 함수
+	return stack[top--]; // stack의 top 요소를 출력하는 함수
 }
 
-precedence get_token() { // expr에서 char을 받아 규칙에 맞추어 변환하는 함수
+precedence get_token(char expr[]) { // expr에서 토큰을 하나씩 읽어 precedence값으로 변환해주는 함수
 	symbol = expr[n++];
 	switch (symbol) {
 	case'(':return lparen;
@@ -40,74 +41,77 @@ precedence get_token() { // expr에서 char을 받아 규칙에 맞추어 변환
 	}
 }
 
-int operation(precedence token, int fir, int sec) { //precedence를 열거형의 규칙에 따라 출력하는 함수
+int operation(precedence token, int fir, int sec) { //precedence의 종류에 따라 연산을 진행하는 함수
 	int tmp = 0;
 	switch (token) {
 	case lparen:
 		break;
 	case rparen:
 		break;
-	case plus:printf("+");
-	    tmp = fir+sec;
-	    return tmp;
+	case plus:
+		tmp = fir + sec;
+		return tmp;
 		break;
-	case minus:printf("-");
-	    tmp = fir-sec;
-	    return tmp;
+	case minus:
+		tmp = fir - sec;
+		return tmp;
 		break;
-	case divide:printf("/");
-	    tmp = fir/sec;
-	    return tmp;
+	case divide:
+		tmp = fir / sec;
+		return tmp;
 		break;
-	case times: printf("*");
-	    tmp = fir*sec;
-	    return tmp;
+	case times: 
+		tmp = fir * sec;
+		return tmp;
 		break;
-	case mod:printf("%%");
-	    tmp = fir%sec;
-	    return tmp;
+	case mod:
+		tmp = fir % sec;
+		return tmp;
 		break;
 	case eos: break;
-	case lg: printf("<");
+	case lg: 
 		break;
-	case rg: printf(">");
+	case rg: 
 		break;
-	caseand : printf("&");
+	case and: 
 		break;
-	case or : printf("|");
+	case or : 
 		break;
 	default: break;
 	}
 }
 
-int main() {
+int main(void) {
 	precedence token = 0;
 	int sec, fir;
 	int number = 0;
 	int tmp = 0;
-    for(int j = 0 ; j<3 ; j++){
-        scanf("%s", expr);
-        printf("postfix expression : %s", expr);
-	    for (int i = 0; i < number; i++) {
-		    token = eos;
-		    stack[0] = eos;
-		    n = 0;
-		    top = 0; // 전역 변수로 선언했기 때문에 시작 전 초기화 해줘야함.
-		    
-		    while ((token = get_token()) != eos) { // token을 받아 eos가 아닌 한 반복하는 조건
-			    if (token == operand) {
-				    tmp = expr[n-1] - 48;
-				    push(tmp);
-			    }else {
-		    		sec = pop();
-			    	fir = pop();
-			    	tmp = operation(token, fir,sec);
-			    	push(tmp);
-			    }
-		    }
-		    printf("\n\n");
-	    }
-    }
-    
+	char expression[3][100] = { "12/3-45*+67*-\0","342*+5-3/21*+57%-\0","123*+45+6*7-+\0" };
+	for (int j = 0; j < 3; j++) {
+		
+		char* expr = expression[j]; // 포인터로 위에 입력된 string을 하나씩 가져옴
+		printf("postfix expression : %s\n", expr);
+		token = eos;
+		stack[0] = eos;
+		n = 0;
+		top = 0; // 전역 변수로 선언했기 때문에 시작 전 초기화 해줘야함.
+
+		while ((token = get_token(expr)) != eos) { // token을 받아 eos가 아닌 한 반복하는 조건
+			if (token == operand) { // operand일 경우 push
+				tmp = expr[n - 1] - 48; 
+				push(tmp);
+			}
+			else { // operand가 아니면 연산자 -> 연산 실행
+				sec = pop(); //차례대로 push 해서 피연산자 대입
+				fir = pop();
+				tmp = operation(token, fir, sec); // 연산
+				push(tmp); // 결과 push
+			}
+		}
+
+	tmp = pop(); // 마지막에 남은 피연산자 == 결과
+	printf("evaluation result: %d\n", tmp); // 결과 출력
+	}
+
 	return 0;
 }
