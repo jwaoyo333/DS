15f63fb41b23faaedc76c5aca9502adb526ea9cb
jwaoyo333 <165109274+jwaoyo333@users.noreply.github.com>
2024-10-07 16:15:34 +0900

    Update queue.c

--- 1007/queue.c
+++ 1007/queue.c
@@ -1,14 +1,14 @@
-//2022428313 ¿ìÁ¤ÁÖ
-#define _CRT_SECURE_NO_WARNINGS
+//2022428313 ìš°ì •ì£¼
+#define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h>
 #include <stdlib.h>
 
 int front = 0;
-int rear = 1;
-int number = 0;
+int rear = 0;
 int max = 0;
+int number = 0;
 
-void addQ(int * queue , int input) {
+void addQ(int* queue, int input) {
 	rear = (rear + 1) % max;
 	if (rear == front) {
 		printf("queue full : cannot add.\n");
@@ -20,12 +20,12 @@ void addQ(int * queue , int input) {
 	return;
 }
 
-int deleteQ(int * queue) {
+int deleteQ(int* queue) {
 	if (front == rear) {
 		printf("queue empty: cannot delete\n");
 		exit(0);
 	}
-	
+
 	front = (front + 1) % max;
 	number--;
 	return queue[front];
@@ -33,9 +33,9 @@ int deleteQ(int * queue) {
 
 void printqueue(int* queue) {
 	printf("Circular queue: ");
-	if (rear <= front) {
-		rear += max;
-		for (int i = front; i < rear; i++) {
+	if (rear < front) {
+		int sub = rear + max;
+		for (int i = front+1; i < sub+1; i++) {
 			if (i >= max) {
 				printf("%d ", queue[i % max]);
 			}
@@ -43,8 +43,23 @@ void printqueue(int* queue) {
 				printf("%d ", queue[i]);
 			}
 		}
-	}else {
-		for (int i = front; i < rear; i++) {
+	}
+	else if (rear == front&&number == 0) {
+		printf("queue is empty\n");
+	}
+	else if (rear == front && number == max-1) {
+		int sub = rear + max;
+		for (int i = front; i < sub; i++) {
+			if (i >= max) {
+				printf("%d ", queue[i % max]);
+			}
+			else {
+				printf("%d ", queue[i]);
+			}
+		}
+	}
+	else {
+		for (int i = front+1; i < rear+1; i++) {
 			printf("%d ", queue[i]);
 		}
 	}
@@ -55,16 +70,19 @@ void printqueue(int* queue) {
 int main(void) {
 
 	int num = 0;
-	FILE* fp = fopen("in1.txt", "r");
+	FILE* fp = fopen("in3.txt", "r");
 	fscanf(fp, "%d", &num);
-	printf("Queue size : %d", num);
-	int* queue = (int*)malloc(sizeof(int) * num);
+	printf("Queue size : %d\n", num);
+	int* queue = (int*)calloc(num,sizeof(int));
 	char input = NULL;
 	int inputnum = 0;
 	int output = 0;
+	max = num;
 	while (!feof(fp)) {
+		fgetc(fp);
 		fscanf(fp, "%c", &input);
 		if (input == 'a') {
+			fgetc(fp);
 			fscanf(fp, "%d", &inputnum);
 			addQ(queue, inputnum);
 			printqueue(queue);
@@ -74,6 +92,7 @@ int main(void) {
 			printqueue(queue);
 		}
 	}
-
+	free(queue);
+	fclose(fp);
 	return 0;
 }
